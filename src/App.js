98637a3c73ff2f52de68fc89766e89477e9d7d33
Harshit Kumar <kumarharshit850@gmail.com>
2021-02-06 22:55:55 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState,useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
+import wordsToNumber from 'words-to-number';
+
 import './App.css';
 import NewsCards from './components/NewsCards/NewsCards';
 import useStyles from './styles.js';
@@ -16,13 +18,23 @@ const App = () => {
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         if(command === 'newHeadlines') {
           // alert('This code was executed');
           setNewsArticles(articles);
           setActiveArticle(-1);
         } else if(command === 'highlight') {
           setActiveArticle((prevActiveArticle) => prevActiveArticle+1);
+        } else if(command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumber(number, { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if(parsedNumber > 20) {
+            alanBtn().playText('Please try that again.');
+          } else if(article) {
+            window.open(articles[number].url, '_blank');
+            alanBtn().playText('Opening...');
+          }
         }
       }
     })
